4c89c82a95ccc1d943c8ae0e77b66e1a0d1b1c86
KFC <160381066+Karl-FC@users.noreply.github.com>
2024-10-03 15:41:13 +0800

    Reworked from Forge to NeoForge

--- src/main/java/net/mcreator/kraftmine/KraftmineMod.java
+++ src/main/java/net/mcreator/kraftmine/KraftmineMod.java
@@ -1,112 +1,82 @@
-/*
- *    MCreator note:
- *
- *    If you lock base mod element files, you can edit this file and it won't get overwritten.
- *    If you change your modid or package, you need to apply these changes to this file MANUALLY.
- *
- *    Settings in @Mod annotation WON'T be changed in case of the base mod element
- *    files lock too, so you need to set them manually here in such case.
- *
- *    If you do not lock base mod element files in Workspace settings, this file
- *    will be REGENERATED on each build.
- *
- */
 package net.mcreator.kraftmine;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
-import net.minecraftforge.network.simple.SimpleChannel;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.common.MinecraftForge;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+import net.neoforged.neoforge.network.handling.IPayloadHandler;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.fml.util.thread.SidedThreadGroups;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.IEventBus;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Tuple;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.FriendlyByteBuf;
 
-import net.mcreator.kraftmine.init.KraftmineModTabs;
-import net.mcreator.kraftmine.init.KraftmineModSounds;
-import net.mcreator.kraftmine.init.KraftmineModPotions;
-import net.mcreator.kraftmine.init.KraftmineModParticleTypes;
-import net.mcreator.kraftmine.init.KraftmineModPaintings;
-import net.mcreator.kraftmine.init.KraftmineModMobEffects;
-import net.mcreator.kraftmine.init.KraftmineModMenus;
-import net.mcreator.kraftmine.init.KraftmineModItems;
-import net.mcreator.kraftmine.init.KraftmineModFluids;
-import net.mcreator.kraftmine.init.KraftmineModFluidTypes;
-import net.mcreator.kraftmine.init.KraftmineModFeatures;
-import net.mcreator.kraftmine.init.KraftmineModEntities;
-import net.mcreator.kraftmine.init.KraftmineModEnchantments;
-import net.mcreator.kraftmine.init.KraftmineModBlocks;
-import net.mcreator.kraftmine.init.KraftmineModBlockEntities;
-import net.mcreator.kraftmine.init.KraftmineModBiomes;
-
-import java.util.function.Supplier;
-import java.util.function.Function;
-import java.util.function.BiConsumer;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Map;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Collection;
 import java.util.ArrayList;
-import java.util.AbstractMap;
 
 @Mod("kraftmine")
 public class KraftmineMod {
 	public static final Logger LOGGER = LogManager.getLogger(KraftmineMod.class);
 	public static final String MODID = "kraftmine";
 
-	public KraftmineMod() {
-		MinecraftForge.EVENT_BUS.register(this);
-		KraftmineModTabs.load();
-		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		KraftmineModSounds.REGISTRY.register(bus);
-		KraftmineModBlocks.REGISTRY.register(bus);
-		KraftmineModItems.REGISTRY.register(bus);
-		KraftmineModEntities.REGISTRY.register(bus);
-		KraftmineModBlockEntities.REGISTRY.register(bus);
-		KraftmineModFeatures.REGISTRY.register(bus);
-		KraftmineModFluids.REGISTRY.register(bus);
-		KraftmineModFluidTypes.REGISTRY.register(bus);
-		KraftmineModPaintings.REGISTRY.register(bus);
-		KraftmineModMobEffects.REGISTRY.register(bus);
-		KraftmineModPotions.REGISTRY.register(bus);
-		KraftmineModEnchantments.REGISTRY.register(bus);
-		KraftmineModParticleTypes.REGISTRY.register(bus);
-		KraftmineModMenus.REGISTRY.register(bus);
-		KraftmineModBiomes.REGISTRY.register(bus);
+	public KraftmineMod(IEventBus modEventBus) {
+		// Start of user code block mod constructor
+		// End of user code block mod constructor
+		NeoForge.EVENT_BUS.register(this);
+		modEventBus.addListener(this::registerNetworking);
+
+		// Start of user code block mod init
+		// End of user code block mod init
 	}
 
-	private static final String PROTOCOL_VERSION = "1";
-	public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(new ResourceLocation(MODID, MODID), () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
-	private static int messageID = 0;
+	// Start of user code block mod methods
+	// End of user code block mod methods
+	private static boolean networkingRegistered = false;
+	private static final Map<CustomPacketPayload.Type<?>, NetworkMessage<?>> MESSAGES = new HashMap<>();
 
-	public static <T> void addNetworkMessage(Class<T> messageType, BiConsumer<T, FriendlyByteBuf> encoder, Function<FriendlyByteBuf, T> decoder, BiConsumer<T, Supplier<NetworkEvent.Context>> messageConsumer) {
-		PACKET_HANDLER.registerMessage(messageID, messageType, encoder, decoder, messageConsumer);
-		messageID++;
+	private record NetworkMessage<T extends CustomPacketPayload>(StreamCodec<? extends FriendlyByteBuf, T> reader, IPayloadHandler<T> handler) {
 	}
 
-	private static final Collection<AbstractMap.SimpleEntry<Runnable, Integer>> workQueue = new ConcurrentLinkedQueue<>();
+	public static <T extends CustomPacketPayload> void addNetworkMessage(CustomPacketPayload.Type<T> id, StreamCodec<? extends FriendlyByteBuf, T> reader, IPayloadHandler<T> handler) {
+		if (networkingRegistered)
+			throw new IllegalStateException("Cannot register new network messages after networking has been registered");
+		MESSAGES.put(id, new NetworkMessage<>(reader, handler));
+	}
+
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	private void registerNetworking(final RegisterPayloadHandlersEvent event) {
+		final PayloadRegistrar registrar = event.registrar(MODID);
+		MESSAGES.forEach((id, networkMessage) -> registrar.playBidirectional(id, ((NetworkMessage) networkMessage).reader(), ((NetworkMessage) networkMessage).handler()));
+		networkingRegistered = true;
+	}
+
+	private static final Collection<Tuple<Runnable, Integer>> workQueue = new ConcurrentLinkedQueue<>();
 
 	public static void queueServerWork(int tick, Runnable action) {
-		workQueue.add(new AbstractMap.SimpleEntry(action, tick));
+		if (Thread.currentThread().getThreadGroup() == SidedThreadGroups.SERVER)
+			workQueue.add(new Tuple<>(action, tick));
 	}
 
 	@SubscribeEvent
-	public void tick(TickEvent.ServerTickEvent event) {
-		if (event.phase == TickEvent.Phase.END) {
-			List<AbstractMap.SimpleEntry<Runnable, Integer>> actions = new ArrayList<>();
-			workQueue.forEach(work -> {
-				work.setValue(work.getValue() - 1);
-				if (work.getValue() == 0)
-					actions.add(work);
-			});
-			actions.forEach(e -> e.getKey().run());
-			workQueue.removeAll(actions);
-		}
+	public void tick(ServerTickEvent.Post event) {
+		List<Tuple<Runnable, Integer>> actions = new ArrayList<>();
+		workQueue.forEach(work -> {
+			work.setB(work.getB() - 1);
+			if (work.getB() == 0)
+				actions.add(work);
+		});
+		actions.forEach(e -> e.getA().run());
+		workQueue.removeAll(actions);
 	}
 }
