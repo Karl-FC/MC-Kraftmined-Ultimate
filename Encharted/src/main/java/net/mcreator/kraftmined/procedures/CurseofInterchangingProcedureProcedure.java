cf9b5d5a255bf10fa9d9a6ca2bd21ed62f19e203
KFC <160381066+Karl-FC@users.noreply.github.com>
2024-10-15 19:27:13 +0800

    Fixed Curse of Interchanging

--- Encharted/src/main/java/net/mcreator/kraftmined/procedures/CurseofInterchangingProcedureProcedure.java
+++ Encharted/src/main/java/net/mcreator/kraftmined/procedures/CurseofInterchangingProcedureProcedure.java
@@ -7,6 +7,7 @@ import net.neoforged.bus.api.Event;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
@@ -16,6 +17,8 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.BlockPos;
 
@@ -37,46 +40,50 @@ public class CurseofInterchangingProcedureProcedure {
 	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity, Entity sourceentity) {
 		if (entity == null || sourceentity == null)
 			return;
+		Entity user = null;
+		Entity target = null;
 		double targetZ = 0;
 		double targetY = 0;
 		double userY = 0;
 		double targetX = 0;
 		double userZ = 0;
 		double userX = 0;
-		Entity user = null;
-		Entity target = null;
+		double extradamage = 0;
 		user = sourceentity;
 		target = entity;
-		if (!(target == user)) {
-			userX = user.getX();
-			userY = user.getY();
-			userZ = user.getZ();
-			targetX = target.getX();
-			targetY = target.getY();
-			targetZ = target.getZ();
-			if (world instanceof Level _level) {
-				if (!_level.isClientSide()) {
-					_level.playSound(null, BlockPos.containing(user.getX(), user.getY(), user.getZ()), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.chorus_fruit.teleport")), SoundSource.PLAYERS, 1, 1);
-				} else {
-					_level.playLocalSound((user.getX()), (user.getY()), (user.getZ()), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.chorus_fruit.teleport")), SoundSource.PLAYERS, 1, 1, false);
+		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).isEnchanted() && (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)
+				.getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, ResourceLocation.parse("kraftmined:curse_of_interchanging")))) != 0) {
+			if (!(target == user)) {
+				userX = user.getX();
+				userY = user.getY();
+				userZ = user.getZ();
+				targetX = target.getX();
+				targetY = target.getY();
+				targetZ = target.getZ();
+				if (world instanceof Level _level) {
+					if (!_level.isClientSide()) {
+						_level.playSound(null, BlockPos.containing(user.getX(), user.getY(), user.getZ()), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.chorus_fruit.teleport")), SoundSource.PLAYERS, 1, 1);
+					} else {
+						_level.playLocalSound((user.getX()), (user.getY()), (user.getZ()), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.chorus_fruit.teleport")), SoundSource.PLAYERS, 1, 1, false);
+					}
 				}
+				{
+					Entity _ent = user;
+					_ent.teleportTo(targetX, targetY, targetZ);
+					if (_ent instanceof ServerPlayer _serverPlayer)
+						_serverPlayer.connection.teleport(targetX, targetY, targetZ, _ent.getYRot(), _ent.getXRot());
+				}
+				{
+					Entity _ent = target;
+					_ent.teleportTo(userX, userY, userZ);
+					if (_ent instanceof ServerPlayer _serverPlayer)
+						_serverPlayer.connection.teleport(userX, userY, userZ, _ent.getYRot(), _ent.getXRot());
+				}
+				if (target instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 40, 1, false, false));
+			} else if (target == user) {
+				target.hurt(new DamageSource(world.holderOrThrow(DamageTypes.MAGIC)), 5);
 			}
-			{
-				Entity _ent = user;
-				_ent.teleportTo(targetX, targetY, targetZ);
-				if (_ent instanceof ServerPlayer _serverPlayer)
-					_serverPlayer.connection.teleport(targetX, targetY, targetZ, _ent.getYRot(), _ent.getXRot());
-			}
-			{
-				Entity _ent = target;
-				_ent.teleportTo(userX, userY, userZ);
-				if (_ent instanceof ServerPlayer _serverPlayer)
-					_serverPlayer.connection.teleport(userX, userY, userZ, _ent.getYRot(), _ent.getXRot());
-			}
-			if (target instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 40, 1, false, false));
-		} else if (target == user) {
-			target.hurt(new DamageSource(world.holderOrThrow(DamageTypes.MAGIC)), 5);
 		}
 	}
 }
