27a2a058ab61f6dbe1d19a4d707571926711ef95
KFC <160381066+Karl-FC@users.noreply.github.com>
2024-10-30 16:04:08 +0800

    Fixed Creative Tab not working

--- Kombat/src/main/java/net/mcreator/kraftmined/init/KraftminedModTabs.java
+++ Kombat/src/main/java/net/mcreator/kraftmined/init/KraftminedModTabs.java
@@ -5,12 +5,15 @@
 package net.mcreator.kraftmined.init;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 import net.mcreator.kraftmined.KraftminedMod;
@@ -18,6 +21,12 @@ import net.mcreator.kraftmined.KraftminedMod;
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class KraftminedModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, KraftminedMod.MODID);
+	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> KRAFTMINE_KOMBAT = REGISTRY.register("kraftmine_kombat",
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.kraftmined.kraftmine_kombat")).icon(() -> new ItemStack(KraftminedModItems.NETHERITE_BATTLEAXE.get())).displayItems((parameters, tabData) -> {
+				tabData.accept(KraftminedModItems.NETHERITE_BATTLEAXE.get());
+				tabData.accept(KraftminedModItems.QUIVER_CHESTPLATE.get());
+				tabData.accept(KraftminedModItems.EYEPATCH_HELMET.get());
+			}).withSearchBar().build());
 
 	@SubscribeEvent
 	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
